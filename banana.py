3479dea7dd2ce1faeb8013c2193fd0b582d3d834
AJHM <AJHM@MacBook-Air-de-Asbel.local>
2020-08-04 10:40:22 -0500

    primer progama

---
+++ banana.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 17 20:57:35 2020
+
+@author: AJHM
+"""
+fruit = 'banana'
+index = 0
+while index < len(fruit):
+    letter = fruit[index]
+    print(letter)
+    index = index + 1
